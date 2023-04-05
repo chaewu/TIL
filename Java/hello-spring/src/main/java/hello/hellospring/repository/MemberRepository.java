383e3c93f3aada0b8e0db90110a2ee0f54627751
chaewoo1002 <chaewoo1002@gmail.com>
2023-04-06 00:57:51 +0900

    ✏️ :: 예제를 통한 회원관리 🧑‍💻 :: Spring 개발 환경 설정 ✏️ :: 가입, 검증, 조회 기능 구현

---
+++ Java/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id); // NULL이 반환 될 가능성이 있을때 그대로 반환하기 보다는 Optional로 감싸서 반환
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
