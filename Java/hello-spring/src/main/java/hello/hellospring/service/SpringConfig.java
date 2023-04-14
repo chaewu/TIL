495b333fc78760128263ff2c2081b1b4960c0612
chaewoo1002 <chaewoo1002@gmail.com>
2023-04-14 13:42:17 +0900

    ✏️ :: 통합 테스트, JdbcTemplate

--- Java/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ Java/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -2,6 +2,7 @@ package hello.hellospring.service;
 
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
